6dc893daca252c1d609aeb33d77a8ca657efed91
tannukurekar <tannukurekar3@gmail.com>
2023-08-11 22:48:09 +0530

    loop program

---
+++ Loop.java
@@ -0,0 +1,10 @@
+public class Loop {  
+    public static void main(String[] args) {  
+        //Code of Java for loop  
+        for(int i=1;i<=10;i++){  
+            System.out.println(i);  
+        } 
+    }
+}
+    
+
